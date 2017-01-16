83fb0d62c06da83d5fbbb6a4379119fe97a53b82
Christian Ferraz Lemos de Sousa <kress@hotmail.com.br>
2017-01-16 15:55:30 -0200

    Update File controller/model/test module aliasing style.

--- apps/software/lib/software/controller/file.ex
+++ apps/software/lib/software/controller/file.ex
@@ -1,22 +1,22 @@
 defmodule Helix.Software.Controller.File do
 
   alias Helix.Software.Repo
-  alias Helix.Software.Model.File, as: MdlFile
+  alias Helix.Software.Model.File, as: File
   import Ecto.Query, only: [where: 3]
 
-  @spec create(MdlFile.creation_params) ::
-    {:ok, MdlFile.t}
+  @spec create(File.creation_params) ::
+    {:ok, File.t}
     | {:error, :file_exists | Ecto.Changeset.t}
   def create(params) do
     params
-    |> MdlFile.create_changeset()
+    |> File.create_changeset()
     |> Repo.insert()
     |> parse_errors()
   end
 
-  @spec find(HELL.PK.t) :: {:ok, MdlFile.t} | {:error, :notfound}
+  @spec find(HELL.PK.t) :: {:ok, File.t} | {:error, :notfound}
   def find(file_id) do
-    case Repo.get_by(MdlFile, file_id: file_id) do
+    case Repo.get_by(File, file_id: file_id) do
       nil ->
         {:error, :notfound}
       file ->
@@ -24,44 +24,44 @@ defmodule Helix.Software.Controller.File do
     end
   end
 
-  @spec update(HELL.PK.t, MdlFile.update_params) ::
-    {:ok, MdlFile.t}
+  @spec update(HELL.PK.t, File.update_params) ::
+    {:ok, File.t}
     | {:error, :file_exists | :notfound | Ecto.Changeset.t}
   def update(file_id, params) do
     with {:ok, file} <- find(file_id) do
       file
-      |> MdlFile.update_changeset(params)
+      |> File.update_changeset(params)
       |> Repo.update()
       |> parse_errors()
     end
   end
 
-  @spec move(MdlFile.t, String.t) ::
-    {:ok, MdlFile.t}
+  @spec move(File.t, String.t) ::
+    {:ok, File.t}
     | {:error, :file_exists | Ecto.Changeset.t}
   def move(file, file_path) do
     file
-    |> MdlFile.update_changeset(%{file_path: file_path})
+    |> File.update_changeset(%{file_path: file_path})
     |> Repo.update()
     |> parse_errors()
   end
 
-  @spec rename(MdlFile.t, String.t) ::
-    {:ok, MdlFile.t}
+  @spec rename(File.t, String.t) ::
+    {:ok, File.t}
     | {:error, :file_exists | Ecto.Changeset.t}
   def rename(file, file_name) do
     file
-    |> MdlFile.update_changeset(%{name: file_name})
+    |> File.update_changeset(%{name: file_name})
     |> Repo.update()
     |> parse_errors()
   end
 
-  @spec copy(MdlFile.t,
+  @spec copy(File.t,
     %{
       name: String.t,
       file_path: String.t,
       storage_id: PK.t}) ::
-        {:ok, MdlFile.t}
+        {:ok, File.t}
         | {:error, :file_exists | Ecto.Changeset.t}
   def copy(file, params) do
     %{
@@ -70,18 +70,18 @@ defmodule Helix.Software.Controller.File do
       storage_id: params.storage_id,
       file_size: file.file_size,
       file_type: file.file_type}
-    |> MdlFile.create_changeset()
+    |> File.create_changeset()
     |> Repo.insert()
     |> parse_errors()
   end
 
-  @spec delete(MdlFile.t) :: no_return
-  def delete(file = %MdlFile{}),
+  @spec delete(File.t) :: no_return
+  def delete(file = %File{}),
     do: delete(file.file_id)
 
   @spec delete(HELL.PK.t) :: no_return
   def delete(file_id) do
-    MdlFile
+    File
     |> where([f], f.file_id == ^file_id)
     |> Repo.delete_all()
 
--- apps/software/test/controller/file_test.exs
+++ apps/software/test/controller/file_test.exs
@@ -5,9 +5,9 @@ defmodule Helix.Software.Controller.FileTest do
   alias HELL.IPv6
   alias HELL.TestHelper.Random, as: HRand
   alias Helix.Software.Repo
-  alias Helix.Software.Model.FileType, as: MdlFileType
-  alias Helix.Software.Controller.Storage, as: CtrlStorage
-  alias Helix.Software.Controller.File, as: CtrlFile
+  alias Helix.Software.Controller.Storage, as: StorageController
+  alias Helix.Software.Controller.File, as: FileController
+  alias Helix.Software.Model.FileType, as: FileType
 
   setup_all do
     file_type = create_file_type()
@@ -15,7 +15,7 @@ defmodule Helix.Software.Controller.FileTest do
   end
 
   setup %{file_type: file_type} do
-    {:ok, s} = CtrlStorage.create()
+    {:ok, s} = StorageController.create()
     payload = create_params(%{file_type: file_type, storage_id: s.storage_id})
     {:ok, payload: payload}
   end
@@ -23,7 +23,7 @@ defmodule Helix.Software.Controller.FileTest do
   defp create_file_type() do
     file_type = HRand.string(min: 20)
     %{file_type: file_type, extension: ".test"}
-    |> MdlFileType.create_changeset()
+    |> FileType.create_changeset()
     |> Repo.insert!()
     file_type
   end
@@ -40,8 +40,8 @@ defmodule Helix.Software.Controller.FileTest do
 
   describe "file creation" do
     test "creates the file", %{payload: payload} do
-      {:ok, file} = CtrlFile.create(payload)
-      {:ok, ^file} = CtrlFile.find(file.file_id)
+      {:ok, file} = FileController.create(payload)
+      {:ok, ^file} = FileController.find(file.file_id)
 
       assert payload.name == file.name
       assert payload.file_path == file.file_path
@@ -51,34 +51,34 @@ defmodule Helix.Software.Controller.FileTest do
     end
 
     test "failure when file exists", %{payload: payload} do
-      {:ok, _} = CtrlFile.create(payload)
-      assert {:error, :file_exists} == CtrlFile.create(payload)
+      {:ok, _} = FileController.create(payload)
+      assert {:error, :file_exists} == FileController.create(payload)
     end
   end
 
   describe "file fetching" do
     test "fetches the file", %{payload: payload} do
-      {:ok, file} = CtrlFile.create(payload)
-      assert {:ok, ^file} = CtrlFile.find(file.file_id)
+      {:ok, file} = FileController.create(payload)
+      assert {:ok, ^file} = FileController.find(file.file_id)
     end
 
     test "failure when file doesn't exist" do
-      assert {:error, :notfound} == CtrlFile.find(IPv6.generate([]))
+      assert {:error, :notfound} == FileController.find(IPv6.generate([]))
     end
   end
 
   describe "file update" do
     test "updates the file", %{payload: payload0} do
-      {:ok, storage} = CtrlStorage.create()
+      {:ok, storage} = StorageController.create()
 
       payload1 = %{
         name: HRand.digits(min: 20),
         file_path: HRand.digits(min: 20),
         storage_id: storage.storage_id}
 
-      {:ok, file0} = CtrlFile.create(payload0)
-      {:ok, file1} = CtrlFile.update(file0.file_id, payload1)
-      {:ok, ^file1} = CtrlFile.find(file0.file_id)
+      {:ok, file0} = FileController.create(payload0)
+      {:ok, file1} = FileController.update(file0.file_id, payload1)
+      {:ok, ^file1} = FileController.find(file0.file_id)
 
       assert payload1.name == file1.name
       assert payload1.file_path == file1.file_path
@@ -86,57 +86,57 @@ defmodule Helix.Software.Controller.FileTest do
     end
 
     test "fails when file doesn't exist" do
-      assert {:error, :notfound} == CtrlFile.update(IPv6.generate([]), %{})
+      assert {:error, :notfound} == FileController.update(IPv6.generate([]), %{})
     end
 
     test "fails when file exists", %{payload: payload0} do
-      CtrlFile.create(payload0)
+      FileController.create(payload0)
 
       p = %{file_type: payload0.file_type, storage_id: payload0.storage_id}
       payload1 = create_params(p)
-      {:ok, file1} = CtrlFile.create(payload1)
+      {:ok, file1} = FileController.create(payload1)
 
-      assert {:error, :file_exists} == CtrlFile.update(file1.file_id, payload0)
-      assert {:ok, ^file1} = CtrlFile.find(file1.file_id)
+      assert {:error, :file_exists} == FileController.update(file1.file_id, payload0)
+      assert {:ok, ^file1} = FileController.find(file1.file_id)
     end
   end
 
   describe "renaming a file" do
     test "renames the file", %{payload: payload} do
-      {:ok, file} = CtrlFile.create(payload)
+      {:ok, file} = FileController.create(payload)
       new_name = Burette.Color.name()
-      {:ok, renamed_file} = CtrlFile.rename(file, new_name)
-      {:ok, ^renamed_file} = CtrlFile.find(renamed_file.file_id)
+      {:ok, renamed_file} = FileController.rename(file, new_name)
+      {:ok, ^renamed_file} = FileController.find(renamed_file.file_id)
 
       assert new_name == renamed_file.name
     end
 
     test "fails to rename when file exists", %{payload: payload0} do
-      {:ok, file0} = CtrlFile.create(payload0)
+      {:ok, file0} = FileController.create(payload0)
 
       payload1 =
         %{file_type: payload0.file_type, storage_id: payload0.storage_id}
         |> create_params()
         |> Map.put(:file_path, payload0.file_path)
-      {:ok, file1} = CtrlFile.create(payload1)
+      {:ok, file1} = FileController.create(payload1)
 
-      assert {:error, :file_exists} == CtrlFile.rename(file1, file0.name)
-      assert {:ok, ^file1} = CtrlFile.find(file1.file_id)
+      assert {:error, :file_exists} == FileController.rename(file1, file0.name)
+      assert {:ok, ^file1} = FileController.find(file1.file_id)
     end
   end
 
   describe "moving a file" do
     test "moves the file", %{payload: payload} do
-      {:ok, file} = CtrlFile.create(payload)
+      {:ok, file} = FileController.create(payload)
       new_path = Burette.Color.name()
-      {:ok, moved_file} = CtrlFile.move(file, new_path)
-      {:ok, ^moved_file} = CtrlFile.find(file.file_id)
+      {:ok, moved_file} = FileController.move(file, new_path)
+      {:ok, ^moved_file} = FileController.find(file.file_id)
 
       assert new_path == moved_file.file_path
     end
 
     test "fails to move when file exists", %{payload: payload0} do
-      {:ok, file0} = CtrlFile.create(payload0)
+      {:ok, file0} = FileController.create(payload0)
 
       payload1 =
         %{file_type: payload0.file_type, storage_id: payload0.storage_id}
@@ -144,24 +144,24 @@ defmodule Helix.Software.Controller.FileTest do
         |> Map.put(:name, payload0.name)
         |> Map.put(:file_path, payload0.name)
 
-      {:ok, file1} = CtrlFile.create(payload1)
+      {:ok, file1} = FileController.create(payload1)
 
-      assert {:error, :file_exists} == CtrlFile.move(file1, file0.file_path)
-      assert {:ok, ^file1} = CtrlFile.find(file1.file_id)
+      assert {:error, :file_exists} == FileController.move(file1, file0.file_path)
+      assert {:ok, ^file1} = FileController.find(file1.file_id)
     end
   end
 
   describe "copying a file" do
     test "copies the file", %{payload: payload0} do
-      {:ok, file0} = CtrlFile.create(payload0)
+      {:ok, file0} = FileController.create(payload0)
 
       payload1 = %{
         storage_id: file0.storage_id,
         file_path: file0.file_path,
         name: Burette.Color.name()}
 
-      {:ok, file1} = CtrlFile.copy(file0, payload1)
-      {:ok, ^file1} = CtrlFile.find(file1.file_id)
+      {:ok, file1} = FileController.copy(file0, payload1)
+      {:ok, ^file1} = FileController.find(file1.file_id)
 
       assert payload1.name == file1.name
       assert file0.file_path == file1.file_path
@@ -171,33 +171,33 @@ defmodule Helix.Software.Controller.FileTest do
     end
 
     test "fails to copy when file exists", %{payload: payload0} do
-      {:ok, file} = CtrlFile.create(payload0)
+      {:ok, file} = FileController.create(payload0)
 
       payload1 = %{
         storage_id: file.storage_id,
         file_path: file.file_path,
         name: file.name}
 
-      assert {:error, :file_exists} == CtrlFile.copy(file, payload1)
-      assert {:ok, ^file} = CtrlFile.find(file.file_id)
+      assert {:error, :file_exists} == FileController.copy(file, payload1)
+      assert {:ok, ^file} = FileController.find(file.file_id)
     end
   end
 
   describe "deleting a file" do
     test "deletes the file", %{payload: payload} do
-      {:ok, file} = CtrlFile.create(payload)
-      :ok = CtrlFile.delete(file)
-      :ok = CtrlFile.delete(file)
+      {:ok, file} = FileController.create(payload)
+      :ok = FileController.delete(file)
+      :ok = FileController.delete(file)
 
-      assert {:error, :notfound} == CtrlFile.find(file.file_id)
+      assert {:error, :notfound} == FileController.find(file.file_id)
     end
 
     test "deleting the file by it's id is idempotency", %{payload: payload} do
-      {:ok, file} = CtrlFile.create(payload)
-      :ok = CtrlFile.delete(file.file_id)
-      :ok = CtrlFile.delete(file.file_id)
+      {:ok, file} = FileController.create(payload)
+      :ok = FileController.delete(file.file_id)
+      :ok = FileController.delete(file.file_id)
 
-      assert {:error, :notfound} == CtrlFile.find(file.file_id)
+      assert {:error, :notfound} == FileController.find(file.file_id)
     end
   end
 end
